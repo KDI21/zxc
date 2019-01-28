779a8391dc250bec4de8c1a84127e70a3de47ca0
AlexAlex-1 <cool.skorov@mail.ru>
2019-01-28 10:52:51 +0300

    asdqwe

--- connect.php
+++ connect.php
@@ -1,4 +1,8 @@
+#qwe;
+#asd;
 <?php
+
+
 $mysqli = new mysqli("127.0.0.1", "skorov", "777", "Project");
 if (mysqli_connect_errno()) {
     printf("Не удалось подключиться: %s\n", mysqli_connect_error());
